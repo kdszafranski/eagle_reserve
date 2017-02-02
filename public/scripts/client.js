6d66a9e7d7c6fa6e57006bf645f40a2002fcde49
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-02-02 10:59:16 -0600

    add routing to client.js

---
+++ public/scripts/client.js
@@ -0,0 +1,34 @@
+//Declare app
+var myApp = angular.module('myApp', ['ngRoute']);
+
+//Config
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/login', {
+      templateUrl: '../views/partials/login.html',
+      controller: 'AuthController',
+    })
+    .when('/home', {
+      templateUrl: '../views/partials/home.html',
+      controller: 'HomeController'
+    })
+    .when('/manage', {
+      templateUrl: '../views/partials/manage.html',
+      controller: 'ManageController'
+    })
+    .when('/reserve', {
+      templateUrl: '../views/partials/reserve.html',
+      controller: 'ReserveController'
+    })
+    .when('/users', {
+      templateUrl: '../views/partials/users.html',
+      controller: 'UsersController'
+    })
+    .when('/items', {
+      templateUrl: '../views/partials/items.html',
+      controller: 'ItemsController'
+    })
+    .otherwise({
+      redirectTo: 'login'
+    }); // end $routeProvider
+}]); // end config
