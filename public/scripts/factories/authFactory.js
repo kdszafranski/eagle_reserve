d1525dfa5179de13191f445b1b75d442137e7e05
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-02-03 15:57:55 -0600

    create authFactory, pulled into authController on login.html

---
+++ public/scripts/factories/authFactory.js
@@ -0,0 +1,29 @@
+myApp.factory('AuthFactory', function ($http) {
+  console.log('in auth factory');
+  
+  var Status = {
+    loggedIn: false,
+  };
+
+  // the public API
+  return {
+    Status: Status,
+
+    checkLoggedIn: function () {
+      return Status.loggedIn;
+    },
+
+    isLoggedIn: function () {
+      return $http.get('/auth');
+    },
+
+    setLoggedIn: function (value) {
+      Status.loggedIn = value;
+    },
+
+    logout: function () {
+      return $http.get('/auth/logout');
+    },
+  };
+
+});
