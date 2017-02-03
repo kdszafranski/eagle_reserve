b40bcbfbbf55e3db177fadf03a71793939932651
Callie Bensel <callie.f.bensel@gmail.com>
2017-02-03 15:21:01 -0600

    added item post

--- public/scripts/controller/ItemsController.js
+++ public/scripts/controller/ItemsController.js
@@ -1,5 +1,21 @@
 myApp.controller( 'ItemsController', [ '$scope', '$http', '$window', function( $scope, $http, $window ){
   console.log( 'in ItemsController' );
 
+  $scope.addItem = function(){
+    var newItem = {
+      newItem: $scope.newItem,
+      category: $scope.categorySelect
+    };
+    $http({
+      method: 'POST',
+      url: '/newItem',
+      data: newItem
+    }).then(function successCallback( response ){
+      console.log( 'response in newItem', response );
+    }, function errorCallback( error ){
+      console.log( 'error occured' );
+    });
+  }; // end addItem 
+
 
 }]); // end ItemsController
