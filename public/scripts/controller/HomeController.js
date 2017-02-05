bc484833647f804454a0ba846919f029e26b07b6
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-02-05 11:00:35 -0600

    add authFactory to home view

--- public/scripts/controller/HomeController.js
+++ public/scripts/controller/HomeController.js
@@ -1,5 +1,17 @@
-myApp.controller( 'HomeController', [ '$scope', '$http', '$window', function( $scope, $http, $window ){
+myApp.controller( 'HomeController', [ '$scope', '$http', '$location', 'AuthFactory',
+function( $scope, $http, $location, AuthFactory){
   console.log( 'in HomeController' );
 
+  //Declare authFactory
+  var authFactory = AuthFactory;
+  //On view load, check if the user is logged in
+  $scope.loggedIn = authFactory.checkLoggedIn();
+  console.log('HC. Logged in:', $scope.loggedIn);
+
+  //If user is not logged in
+  if(!$scope.loggedIn) {
+    //Reroute them to the login page
+    $location.path("/#!/login");
+  } // end if
 
 }]); // end HomeController
