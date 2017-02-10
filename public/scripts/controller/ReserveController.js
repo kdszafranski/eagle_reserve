1018b98f53cc738ad05385df5896534baaaecc7f
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-02-10 17:49:48 -0600

    clean up syntax errors, add clarifying comments

--- public/scripts/controller/ReserveController.js
+++ public/scripts/controller/ReserveController.js
@@ -16,42 +16,40 @@ function( $scope, $http, $location, AuthFactory, $uibModal ){
     $location.path("/#!/login");
   } // end if
 
+  //GET all items from the database
   $http({
-  method: 'GET',
-  url: '/private/items'
+    method: 'GET',
+    url: '/private/items'
   }).then(function(response) {
-  console.log('response for items ->', response);
-
-
-  $scope.items = response.data.results;
-  $scope.categories = $scope.items.map (function (x){
-    return x.category
-  })
-
+    console.log('response for items ->', response);
+    $scope.items = response.data.results;
+    $scope.categories = $scope.items.map (function (x){
+      return x.category;
+    }); // end callback
   }); //end http
 
+  //set periodArray and selection scopes
   $scope.periodArray = ['BS', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'AS'];
   $scope.selection = [];
 
+  //pull the username in from authFactory and store as variable
   var username = authFactory.username;
 
   $scope.testme = function() {
     console.log($scope.newReservation.itemIn);
-  }
-
+  }; // end testme
 
   $scope.newReservation = {
-      categoryIn: '',
-      itemIn: '',
-      dateIn: '',
-      periodIn: $scope.selection,
-      username: username,
-      numberOfStudents: $scope.numberOfStudentsIn,
-      roomNumber: $scope.roomNumberIn
-    };
+    categoryIn: '',
+    itemIn: '',
+    dateIn: '',
+    periodIn: $scope.selection,
+    username: username,
+    numberOfStudents: $scope.numberOfStudentsIn,
+    roomNumber: $scope.roomNumberIn
+  }; // end newReservation
 
-
-    $scope.toggleSelection = function toggleSelection(periodName) {
+  $scope.toggleSelection = function toggleSelection(periodName) {
     var idx = $scope.selection.indexOf(periodName);
 
     // Is currently selected
@@ -65,91 +63,70 @@ function( $scope, $http, $location, AuthFactory, $uibModal ){
     }
   };
 
-
-
-//open the modal (returns a modal instance)
-$scope.open = function (size, newReservation) {
-  //open the modal
-  console.log('Open confirm reservation modal');
-  //set the modalInstance
-  var modalInstance = $uibModal.open({
-    templateUrl: "confirmReservationModal.html",
-    controller: 'ConfirmReservationModalController',
-    size: size,
-    //pass the userId to DeleteUserModalController
-    resolve: {
-      newReservation: function () {
-        return newReservation;
-      } // end userId
-    } // en
-  }); // end modalInstance
-  modalInstance.closed.then(function(){
-  var item = $scope.newReservation.itemIn;
-  var date = $scope.newReservation.dateIn;
-  var period = $scope.newReservation.periodIn;
-  $scope.roomNumberIn = '';
-  $scope.numberOfStudentsIn = '';
-
-
-
-  $scope.outputDiv += '<p>' + 'You have added a reservation for ' + item + ' on ' + date + ' for period ' + period + '</p>';
-});
-}; // end open
+  //open the modal (returns a modal instance)
+  $scope.open = function (size, newReservation) {
+    //open the modal
+    console.log('Open confirm reservation modal');
+    //set the modalInstance
+    var modalInstance = $uibModal.open({
+      templateUrl: "confirmReservationModal.html",
+      controller: 'ConfirmReservationModalController',
+      size: size,
+      //pass the userId to DeleteUserModalController
+      resolve: {
+        newReservation: function () {
+          return newReservation;
+        } // end userId
+      } // end resolve
+    }); // end modalInstance
+    //when the modal instance is closed, show the confirmation alert
+    modalInstance.closed.then(function(){
+      var item = $scope.newReservation.itemIn;
+      var date = $scope.newReservation.dateIn;
+      var period = $scope.newReservation.periodIn;
+      $scope.roomNumberIn = '';
+      $scope.numberOfStudentsIn = '';
+      $scope.outputDiv += '<p>' + 'You have added a reservation for ' + item + ' on ' + date + ' for period ' + period + '</p>';
+    }); // end modalInstance.closed callback
+  }; // end open
 
 }]); // end ReserveController
 
 // confirmation modal
-
 myApp.controller('ConfirmReservationModalController', ['$scope', '$http', '$uibModalInstance', 'newReservation',
 function ($scope, $http, $uibModalInstance, newReservation) {
-    console.log('in ConfirmReservationModalController');
-    console.log(newReservation);
+  console.log('in ConfirmReservationModalController', newReservation);
 
-    $scope.makeReservation = function (){
-      console.log('In Make Reservation');
-
-      $http ({
-        method: 'POST',
-        url: '/private/reservations',
-        data: newReservation,
-      }).then(function(response) {
-              console.log('response ->', response);
-              $scope.sendEmail();
-              $scope.close();
-
-      });
-
-      $http({
-      method: 'GET',
-      url: '/private/reservations'
-  }).then(function(response) {
-      console.log('response ->', response);
-  }); //end http
-
-
-
+  $scope.makeReservation = function (){
+    console.log('In Make Reservation', newReservation);
+    $http ({
+      method: 'POST',
+      url: '/private/reservations',
+      data: newReservation,
+    }).then(function(response) {
+      console.log('makeReservation response ->', response);
+      $scope.sendEmail();
+      $scope.close();
+    }); // end $http
   };//end make Reservation
 
   // send email
   $scope.sendEmail = function( req, res ) {
-      //post info to the server
-      $http({
-        method: 'POST',
-        url: '/private/email'
-      }).then(function(response) {
-        console.log('EMAIL SENT');
+    //post info to the server
+    $http({
+      method: 'POST',
+      url: '/private/email'
+    }).then(function(response) {
+      console.log('sendEmail response-->', response);
+    }).catch(function(err) {
+      //if there was an error, log it
+      console.log(err);
+    }); // end $http
+  }; // end sendEmails
 
-
-      }).catch(function(err) {
-        //if there was an error, log it
-        console.log(err);
-      }); // end $http
-    }; // end sendEmails
-
-    //close the  modal
-    $scope.close = function () {
-      $uibModalInstance.dismiss('cancel');
-    }; // end close
-
+  //close the  modal
+  $scope.close = function () {
+    $uibModalInstance.dismiss('cancel');
+  }; // end close
 
 }]); // end ConfirmReservationModalController
