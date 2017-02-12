73997594d055c4376101152bdb20ae577f3a52bf
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-02-12 12:18:46 -0600

    add confirmation modal to manage reservations page

--- public/scripts/controller/ManageController.js
+++ public/scripts/controller/ManageController.js
@@ -1,5 +1,5 @@
-myApp.controller( 'ManageController', [ '$scope', '$http', '$location', 'AuthFactory',
-function( $scope, $http, $location, AuthFactory ){
+myApp.controller( 'ManageController', [ '$scope', '$http', '$location', 'AuthFactory', '$uibModal',
+function( $scope, $http, $location, AuthFactory, $uibModal ){
   console.log( 'in ManageController' );
 
   //Declare authFactory
@@ -15,6 +15,41 @@ function( $scope, $http, $location, AuthFactory ){
     //Reroute them to the login page
     $location.path("/#!/login");
   } // end if
+
+  //Set category filters
+  $scope.category = [
+    {category: "Lab"},
+    {category: "Cart"},
+    {category: "Equipment"}
+  ]; // end $scope.category
+
+  //Set default filter scope values
+  $scope.teacherSelected= {name: undefined};
+  $scope.itemSelected= {newItem: undefined};
+  $scope.categorySelected= {category: undefined};
+
+  //Initialize datepicker popup to closed
+  $scope.popup = {
+    opened: false
+  }; // end popup
+
+  // delete a reservation (admin view)
+  var deleteReservation = function( indexIn ){
+    $http.delete( '/private/reservations/' + $scope.reservations[ indexIn ]._id )
+    .then(function( response ){
+      console.log( 'delete hit', response );
+      $scope.displayReservation();
+    });// end then
+  };// end deleteReservation
+
+  // delete a reservation (teacher view)
+  var deleteReservationUser = function( indexIn ){
+    $http.delete( '/private/reservations/' + $scope.userReservations[ indexIn ]._id )
+    .then(function( response ){
+      console.log( 'delete hit', response );
+      $scope.getByUsername();
+    });// end then
+  };// end deleteReservation
 
   // GET all reservations to display on DOM
   $scope.displayReservation = function(){
@@ -37,27 +72,6 @@ function( $scope, $http, $location, AuthFactory ){
       $scope.items = response.data.results;
     });// end then
   };// end getItems
-
-  // Getting all the Items in the database and making them selectable
-  $scope.getItems();
-
-  // delete a reservation
-  $scope.deleteReservation = function( indexIn ){
-    $http.delete( '/private/reservations/' + $scope.reservations[ indexIn ]._id )
-    .then(function( response ){
-      console.log( 'delete hit', response );
-      $scope.displayReservation();
-    });// end then
-  };// end deleteReservation
-
-  // delete a reservation
-  $scope.deleteReservationUser = function( indexIn ){
-    $http.delete( '/private/reservations/' + $scope.userReservations[ indexIn ]._id )
-    .then(function( response ){
-      console.log( 'delete hit', response );
-      $scope.getByUsername();
-    });// end then
-  };// end deleteReservation
 
   // getting only reservations for the teacher logged in
   $scope.getByUsername = function (){
@@ -84,19 +98,6 @@ function( $scope, $http, $location, AuthFactory ){
     }); // end $http
   }; // end getTeachers
 
-  // getting all the teachers
-  $scope.getTeachers();
-
-  $scope.category = [
-    {category: "Lab"},
-    {category: "Cart"},
-    {category: "Equipment"}
-  ];
-
-  $scope.teacherSelected= {name: undefined};
-  $scope.itemSelected= {newItem: undefined};
-  $scope.categorySelected= {category: undefined};
-
   $scope.getByDate = function(date){
     console.log('In getByDate');
     //use moment.js to format date chosen
@@ -110,14 +111,56 @@ function( $scope, $http, $location, AuthFactory ){
     });
   };
 
-  $scope.popup = {
-    opened: false
-  }; // end popup
+  var init = function() {
+    // Getting all the Items in the database and making them selectable
+    $scope.getItems();
+    //Get teacher names to populate teacher select filter
+    $scope.getTeachers();
+  }; // end init
 
   $scope.openDatepick = function() {
     //Open the datepicker popup
     $scope.popup.opened = true;
   }; // end openDatepick
 
+  //open the modal (returns a modal instance)
+  $scope.openConfirmationModal = function (size, reservationIndex) {
+    console.log('Open confirm delete modal', reservationIndex);
+    //set the modalInstance
+    var modalInstance = $uibModal.open({
+      templateUrl: "deleteReservationConfirmModal.html",
+      controller: 'DeleteReservationConfirmModalController',
+      size: size
+    }); // end modalInstance
+    //Actions to take once the modal is has been closed:
+    modalInstance.result.then(function (reason) {
+      console.log('reason-->', reason.value);
+      //if the modal was closed via 'confirm' btn, delete the reservation
+      if (reason.value === 'confirm') {
+        //if the user is an admin, run this function:
+        if ($scope.isAdmin) {
+          deleteReservation( reservationIndex );
+        } else {
+          //if the user is a teacher, run this function:
+          deleteReservationUser(reservationIndex);
+        } // end else
+      } // end if
+    }); // end modal result
+  }; // end open
+
+  //Initialize the view
+  init();
 
 }]); // end ManageController
+
+// DeleteReservationConfirmModalController
+myApp.controller('DeleteReservationConfirmModalController', ['$scope', '$uibModalInstance',
+ function ($scope, $uibModalInstance) {
+  console.log('in DeleteReservationConfirmModalController');
+
+  //close the  modal
+  $scope.close = function (reason) {
+    $uibModalInstance.close({ value: reason });
+  }; // end close
+
+}]); // end DeleteReservationConfirmModalController
