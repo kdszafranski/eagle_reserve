26e0f5e1c987c7c1b76ba0d845de6d3bf5a85fde
Kris Szafranski <kdszafranski@gmail.com>
2017-05-11 13:36:32 -0500

    fixing ssl connection option

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,7 +8,7 @@ var connStr = '';
 if(process.env.MONGODB_URI !== undefined) {
     console.log('env connection string');
     connStr = process.env.MONGODB_URI;
-    mongoose.defaults.ssl = true;
+    mongoose.options.ssl = true;
 } else {
     connStr = 'mongodb://localhost:27017/eagle-reserve';
 } // end else
